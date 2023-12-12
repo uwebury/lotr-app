eff3e8261e12cd61f486c7bf9f59d1071fb7a90c
uwebury <142432719+uwebury@users.noreply.github.com>
2023-12-12 12:19:27 +0100

    Imported introduction on index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Link from "next/link";
+import { introduction } from "../resources/lib/data.js";
 
 export default function Homepage() {
   return (
     <>
       <h1>Lord of the Rings: All Volumes</h1>
+      <p>{introduction}</p>
       <ul>
         <li>
           <Link href="/volumes">All Volumes</Link>
