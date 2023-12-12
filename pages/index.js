6133df50ab057f0306553e20dd880133483d791d
uwebury <142432719+uwebury@users.noreply.github.com>
2023-12-12 12:21:47 +0100

    Added all volumes link list

--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,20 @@ export default function Homepage() {
     <>
       <h1>Lord of the Rings: All Volumes</h1>
       <p>{introduction}</p>
+      <h2>All Volumes</h2>
       <ul>
         <li>
-          <Link href="/volumes">All Volumes</Link>
+          <Link href="/volumes/the-fellowship-of-the-ring">
+            The Fellowship of the Ring
+          </Link>
+        </li>
+        <li>
+          <Link href="/volumes/the-two-towers">The Two Towers</Link>
+        </li>
+        <li>
+          <Link href="/volumes/the-return-of-the-king">
+            The Return of the King
+          </Link>
         </li>
       </ul>
     </>
